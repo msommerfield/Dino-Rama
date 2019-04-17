4d1cc33a1b5250e997cf5b0bcc29becd701968d4
Mandy <mandysommerfield@gmail.com>
2019-04-17 12:32:42 -0400

    add default value for returned fields in models.py

--- dino_app/models.py
+++ dino_app/models.py
@@ -3,20 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Diet(models.Model):
-    diet = models.CharField(max_length=255)
+    diet = models.CharField(max_length=255, default="omnivore")
 
     def __str__(self):
         return self.diet
 
 class Location(models.Model):
-    time_period = models.CharField(max_length=255)
+    time_period = models.CharField(max_length=255, default="Cretaceous period")
     region = models.CharField(max_length=255)
 
     def __str__(self):
         return self.time_period
 
 class Dinosaur(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, default="Tyrannosaurus Rex")
     estimated_height = models.CharField(max_length=255)
     estimated_mass = models.CharField(max_length=255)
     image = models.CharField(max_length=400)
