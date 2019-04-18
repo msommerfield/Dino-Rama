25a2a002fc282051f51cfc40c131c501ca1b36e1
Mandy <mandysommerfield@gmail.com>
2019-04-18 13:44:34 -0400

    add veiws to urls.py in app folder

--- dino_app/urls.py
+++ dino_app/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path, include
+from rest_framework import routers
+
 from . import views
 
+router = routers.DefaultRouter()
+router.register('dinosaurs', views.DinosaurView)
+router.register('diets', views.DietView)
+router.register('locations', views.LocationView)
+
+
 urlpatterns = [
+    path('', include(router.urls))
 ]
