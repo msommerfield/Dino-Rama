be68e8a2b54f7b5421b9d6ae2549529e7dccb173
Mandy <mandysommerfield@gmail.com>
2019-04-18 13:38:17 -0400

    add serializers and views

---
+++ dino_app/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from .models import Location, Diet, Dinosaur
+
+class DinosaurSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dinosaur
+        fields = ('name', 'estimated_height', 'estimated_mass', 'image', 'fossil', 'location', 'diet')
+
+class DietSerializer(serializers.ModelSerializer):
+    dinosaurs = DinosaurSerializer(many=True, read_only=True)
+    class Meta:
+        model = Diet
+        fields = ('id', 'diet')
+
+class LocationSerializer(serializers.ModelSerializer):
+    dinosaurs = DinosaurSerializer(many=True, read_only=True)
+    class Meta:
+        model = Location
+        fields = ('id', 'time_period', 'region')
--- dino_app/views.py
+++ dino_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import DinosaurSerializer, DietSerializer, LocationSerializer
+from .models import Dinosaur, Diet, Location
 # Create your views here.
+
+class DinosaurView(viewsets.ModelViewSet):
+    queryset = Dinosaur.objects.all()
+    serializer_class = DinosaurSerializer
+
+class DietView(viewsets.ModelViewSet):
+    queryset = Diet.objects.all()
+    serializer_class = DietSerializer
+
+class LocationView(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
